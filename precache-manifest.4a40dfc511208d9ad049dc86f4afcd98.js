746a6566990d8bca109369643115cd191a2cf974
ksukhaldun <kseniya.khaldun@gmail.com>
2020-01-06 16:21:52 +0300

    Updates

---
+++ precache-manifest.4a40dfc511208d9ad049dc86f4afcd98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4a3c96f01cf9bcb0d0498c49bbcb1e",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "2f135558cb4def4f974b",
+    "url": "/react_people-table/static/css/main.eb80170d.chunk.css"
+  },
+  {
+    "revision": "ba9773a4aaf0c0d92eff",
+    "url": "/react_people-table/static/js/2.6bbfc539.chunk.js"
+  },
+  {
+    "revision": "2f135558cb4def4f974b",
+    "url": "/react_people-table/static/js/main.c9b9fcc7.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
